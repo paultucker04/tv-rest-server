64bfd60adbcb68ce8a997d9db206cc750b673ae4
Paul Tucker <Paultucker04@aol.com>
2019-07-08 21:24:08 -0400

    initial commit

---
+++ api/controllers/tvController.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let smartcast = require('vizio-smart-cast');
+let tv = new smartcast('192.168.1.228', 'Zw49i3hrlv');
+
+exports.power_on = function(req, res) {
+  tv.control.power.on();
+  res.json({ message: 'Powered On' });
+};
+
+exports.power_off = function(req, res) {
+  tv.control.power.off();
+  res.json({ message: 'Powered Off' });
+};
+
+
+exports.pause = function(req, res) {
+  tv.control.media.pause();
+  res.json({ message: 'Paused' });
+};
+
+
+exports.play = function(req, res) {
+  tv.control.media.play();
+  res.json({ message: 'Played' });
+};
---
+++ api/routes/tvRoutes.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(app) {
+  var tvController = require('../controllers/tvController');
+
+  app.route('/tv/poweron')
+    .post(tvController.power_on);
+  app.route('/tv/poweroff')
+    .post(tvController.power_off);
+  app.route('/tv/pause')
+    .post(tvController.pause);
+  app.route('/tv/play')
+    .post(tvController.play);
+
+};
